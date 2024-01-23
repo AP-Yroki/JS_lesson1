5cdb4d6925dc0d3aec72f4f37fd52cb238ab075a
AP-Yroki <144231060+AP-Yroki@users.noreply.github.com>
2024-01-23 12:01:22 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,145 @@
+// let name = prompt('Your name', 'Vasya'); 
+
+// alert(`Привет, ${name}!`); 
+
+// Постфиксная и префиксная формы
+
+// let a = 2, b = 2;
+// let c = 2;
+// let d = 1;
+
+// Результат присваивания
+// a = 4
+// x = 5
+
+// Преобразование типов
+// 1. 1
+// 2. -1
+// 3. 1
+// 4. 2
+// 5. 6
+// 6. 10
+// 7. 10
+// 8. 45
+// 9. NaN
+// 10. -95
+// 11. 95
+// 12. 1
+// 13. NaN
+// 14. -1
+
+
+// Исправьте сложение
+
+// let a = prompt('Первое число?', 1);
+// let b = prompt('Второе число',2);
+
+// let sum = Number(a) + Number(b);
+
+// alert(sum);
+
+
+// Работа с переменными
+
+// Задача 1
+// let num = 3;
+// alert(num);
+
+// Задача 2
+// let a = 10;
+// let b = 2;
+// alert("Сумма: " + (a + b) + "\nРазность: " + (a - b) + "\nПроизведение: " + (a * b) + "\nЧастное: " + (a / b));
+
+// Задача 3
+// let c = 15;
+// let d = 2;
+// let result = c + d;
+// alert(result);
+
+// Задача 4
+// let a1 = 10, b1 = 2, c1 = 5;
+// alert("Сумма: " + (a1 + b1 + c1));
+
+// Задача 5
+// let a2 = 17, b2 = 10;
+// let c2 = a2 - b2;
+// let d2 = 7;
+// let result2 = c2 + d2;
+// alert(result2);
+
+// Работа со строками
+
+// Задача 1
+// let str = 'Привет, Мир!';
+// alert(str);
+
+// Задача 2
+// let str1 = 'Привет, ';
+// let str2 = 'Мир!';
+// alert(str1 + str2);
+
+// Задача 3
+// let name = 'Вован'; 
+// alert('Привет, ' + name + '!');
+
+// Задача 4
+// let age = '987'; 
+// alert('Мне ' + age + ' лет!');
+
+// Функция prompt
+
+// let name = prompt('Введите ваше имя!', 'Михаил')
+// alert('Ваше имя' + name)
+
+// let num = prompt('Введите число', 123)
+// let square = num * num
+// alert('Квадрат:' + square)
+
+// Обращение к символам строки
+// Задача 1
+// let str = 'abcde';
+// alert('Символ a: ' + str[0] + '\nСимвол c: ' + str[2] + '\nСимвол e: ' + str[4]);
+
+// Задача 2
+// let num = '12345';
+// let result = Number(num[0]) * Number(num[1]) * Number(num[2]) * Number(num[3]) * Number(num[4]);
+// alert('Произведение цифр числа ' + num + ': ' + result);
+
+// Практика
+
+// Задача 1
+// let Hour = 60 * 60;
+// let Day = Hour * 24;
+// let Month = Day * 30; 
+
+// alert('Количество секунд в часе: ' + Hour + '\nКоличество секунд в сутках: ' + Day +
+//   '\nКоличество секунд в месяце: ' + Month);
+
+// Задача 2
+// let hour = 12;
+// let minute = 34;
+// let second = 56;
+
+// alert('Текущее время: ' + hour + ':' + minute + ':' + second);
+
+// Задача 3
+// let num = 5; 
+// let square = num * num;
+// alert(num + ' в квадрате равно: ' + square);
+
+// Работа с присваиванием и декрментами
+// Задача 1
+// let num = 47;
+// num += 7;
+// num -= 18;
+// num *= 10;
+// num /= 15;
+
+// alert(num);
+
+// Задача 2
+// let num = 10;
+// num++;
+// num++;
+// num--;
+// alert(num);
